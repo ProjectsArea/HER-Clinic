1ef76405f4868d48ec535ccf343cd645dd67b091
AarthiDatapro <aarthi@datapro.in>
2025-08-04 18:18:37 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Doctor from './components/Doctor';
 import Testimonials from './components/Testimonials';
 import Gallery from './components/Gallery';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
       <BabyTreatments />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   );
 }
